75cdba785240d181397e386e42b9690f166beacf
ThunderSSGSS <ojamesartur@gmail.com>
2025-02-01 02:48:55 +0200

    feat: add feat check branchs2

--- build.sh
+++ build.sh
@@ -22,7 +22,7 @@ else
   echo "$STAGING_DOT_ENV" >> .env; 
 fi
 
-docker build -t ${DOCKER_USERNAME}/${IMAGE_NAME}:${IMAGE_TAG_PREFIX}${VERSION}-${IMAGE_TYPE} .
+docker build -t ${DOCKER_USERNAME}/${IMAGE_NAME}:${IMAGE_TAG_PREFIX}${VERSION}-${ACTUAL_BRANCH} .
 echo "IMAGE_REPOSITORY=${DOCKER_USERNAME}/${IMAGE_NAME}" >> build.env
-echo "IMAGE_TAG=${IMAGE_TAG_PREFIX}${VERSION}-${IMAGE_TYPE}" >> build.env
+echo "IMAGE_TAG=${IMAGE_TAG_PREFIX}${VERSION}-${ACTUAL_BRANCH}" >> build.env
 echo "ID=${RANDOM}" >> build.env
