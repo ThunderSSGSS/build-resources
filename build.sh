6277a284459944b841b4ec85b54551a8be610140
ThunderSSGSS <ojamesartur@gmail.com>
2025-02-01 02:47:33 +0200

    feat: add feat check branchs

--- build.sh
+++ build.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+if [ ! $2 ]; then
+  echo "ERROR: This command needs arguments
+Example: $0 <prod_branch> <actual_branch>
+"
+    exit 1;
+fi
+
+export PROD_BRANCH="$1"
+export ACTUAL_BRANCH="$2"
 export DOCKERFILE_DIR="${DOCKERFILE_DIR:-'.'}"
 export VERSION="$(bash ./version.sh)"
 
@@ -7,7 +16,7 @@ cd $DOCKERFILE_DIR
 
 echo "$DOT_ENV" > .env
 
-if [ "$IMAGE_TYPE" = "prod" ]; then 
+if [ "$ACTUAL_BRANCH" = "$PROD_BRANCH" ]; then 
   echo "$PROD_DOT_ENV" >> .env; 
 else
   echo "$STAGING_DOT_ENV" >> .env; 
